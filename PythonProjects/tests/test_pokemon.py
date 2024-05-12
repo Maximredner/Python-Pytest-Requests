68e8c182020016d190910a8f75956da3d3361cc9
Maximredner <166320162+Maximredner@users.noreply.github.com>
2024-05-12 12:10:13 +0300

    Add files via upload

--- PythonProjects/tests/test_pokemon.py
+++ PythonProjects/tests/test_pokemon.py
@@ -15,7 +15,7 @@ def test_part_of_response():
     assert response_get.json()['data'][0]['name'] == 'Бульбазавр'
 
 
-@pytest.mark.parametrize('key, value', [('name'), ('Бульбазавр'), ('trainer_id', TRAINER_ID), ('id', '26936')])
+@pytest.mark.parametrize('key, value', [('name', 'Бульбазавр'), ('trainer_id', TRAINER_ID), ('id', '26939')])
 def test_parametrize(key, value):
     response_parametrize = requests.get(url = f'{URL}/pokemons',params = {'trainer_id' : TRAINER_ID})
     assert response_parametrize.json()['data'][0][key] == value
